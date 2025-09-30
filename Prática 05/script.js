3db6a49d076e81768bc8350d654f8a6d73f427d0
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2025-09-30 15:28:28 -0300

    Add files via upload

---
+++ Prática 05/script.js
@@ -0,0 +1,11 @@
+let fruits = ["bananaz", "apples", "watermelons"];
+console.log(fruits);
+console.log(fruits[1])
+
+let numbers = new Array(10, 2, 6, 7);
+console.log(numbers);
+
+let EmptyArray = [];
+console.log(EmptyArray);
+EmptyArray[0] = "Juh";
+console.log(EmptyArray);
