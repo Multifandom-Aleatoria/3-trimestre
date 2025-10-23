42fe4cf48615c3a6067a7cf17481166c8c6df264
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2025-10-23 15:49:25 -0300

    Add files via upload

---
+++ PráticaInput/Script.js
@@ -0,0 +1,10 @@
+function sim() {
+  const email = document.getElementById("text").value;
+  const senha = document.getElementById("password").value;
+
+  if (email === "Sim@gmail.com" && senha === "SHELVISIONS4ranghae") {
+    window.location.href = "TelasupersecretaHAHAHA.html";
+  } else {
+    window.alert("Senha ou email incorretos");
+  }
+}
